46a56587932620bfa7829ffe1313687b1f291a7e
Michael Serrato <programador-experto97@hotmail.com>
2017-01-11 17:32:46 -0600

    Method $digest'

--- controllers.js
+++ controllers.js
@@ -1,18 +1,14 @@
 var app = angular.module('mainModule', [])
 
-app.filter('removeHTML', function() {
-  return function(text) {
-    return String(text).replace(/<[^>]+>/gm, '')
-  }
-})
-
-app.controller('FiltersController', [
-  '$scope',
+app.controller('FirstController', [
+  '$scope', 
   function($scope) {
-    $scope.myHTML = '<h1>Hello World</h1>'
-    $scope.myJSON = {}
-    $scope.myJSON.title = 'Some title'
-    $scope.myJSON.body = 'Some body'
-    $scope.myCurrency = 2
+    $scope.name = 'Michael';
+
+    setTimeout(function() {
+      $scope.name = 'Serrato';
+      console.log($scope.name);
+      $scope.$digest();
+    }, 2000);
   }
 ])
--- index.php
+++ index.php
@@ -2,15 +2,13 @@
 <html ng-app="mainModule">
 <head>
   <meta charset="utf-8">
-  <title>FiltersController</title>
+  <title>FirstController</title>
   <link rel="stylesheet" type="text/css" href="style.css?v=<?php echo time(); ?>">
   <script type="text/javascript" src="angular.js?v=<?php echo time(); ?>"></script>
   <script type="text/javascript" src="angular-local-storage.js?v=<?php echo time(); ?>"></script>
   <script type="text/javascript" src="controllers.js?v=<?php echo time(); ?>"></script>
 </head>
-<body ng-controller="FiltersController">
-  <p>{{ myHTML | removeHTML }}</p>
-  <p>{{ myJSON | json }}</p>
-  <p>{{ myCurrency | currency: '$' }}
+<body ng-controller="FirstController">
+  <p>{{ name }}</p>
 </body>
 </html>
