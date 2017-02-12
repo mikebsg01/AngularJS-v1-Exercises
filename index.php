d7e7f323bba527cdd068e1da8be31daaf9cbfd57
Michael Serrato <programador-experto97@hotmail.com>
2017-02-11 20:39:29 -0600

    ToDoList with Services

--- index.php
+++ index.php
@@ -8,7 +8,28 @@
   <script type="text/javascript" src="angular-local-storage.js?v=<?php echo time(); ?>"></script>
   <script type="text/javascript" src="controllers.js?v=<?php echo time(); ?>"></script>
 </head>
-<body ng-controller="FirstController">
-  {{ name }}
+<body ng-controller="ToDoController">
+<div class="block block-2" ng-repeat="activity in ToDoActivities">
+  <p>{{ activity.description }}</p>
+  <small>{{ activity.date | date: 'short' }}<small>
+  <a href="#" ng-click="removeActivity(activity)">Delete</a>
+</div>
+<div class="block block-1">
+  <form ng-submit="addActivity()">
+    <label>Descripción</label>
+    <br>
+    <input type="text" ng-model="newActivity.description">
+    <br>
+    <br>
+    <label>Fecha</label>
+    <br>
+    <input type="datetime-local" ng-model="newActivity.date">
+    <br>
+    <br>
+    <input type="submit" value="Save Activity">
+  </form>
+  <br>
+  <button ng-click="cleanActivities()">Limpiar</button>
+</div>
 </body>
 </html>
