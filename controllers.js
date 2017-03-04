bac7c2509a536fbc461e61570ff4004d87bbf699
Michael Serrato <programador-experto97@hotmail.com>
2017-03-03 20:21:54 -0600

    Creating Custom Directives - Part #2 - The Best Practice

--- controllers.js
+++ controllers.js
@@ -1,5 +1,50 @@
 var app = angular.module('CustomDirective', [])
 
+app.directive('myAutocomplete', function() {
+
+  function link(scope, element, attrs) {
+
+    $(element).autocomplete({
+      source: scope.$eval(attrs.myAutocomplete),
+      select: function(event, ui) {
+        event.preventDefault()
+
+        if (ui.item) {
+          scope.optionSelected(ui.item.value)
+        }
+      },
+      focus: function(event, ui) {
+        event.preventDefault()
+        $(this).val(ui.item.label)
+      }
+    })
+  }
+
+  return {
+    link: link
+  }
+})
+
+app.directive('clearInput', function() {
+
+  function link(scope, element, attrs) {
+
+    $(element).click(function(event) {
+      event.preventDefault()
+      
+      scope.$apply(function() {
+        scope.repoNameSearch = null
+      })
+
+      $(element).siblings('input').val('')
+    })
+  }
+
+  return {
+    link: link
+  }
+})
+
 app.directive('imgCircle', function() {
   return function($scope, $element, attrs) {
     attrs.$observe('imgCircle', function(val) {
@@ -15,13 +60,25 @@ app.directive('imgCircle', function() {
 app.controller('AppCtrl', [
   '$scope', '$http',
   function ($scope, $http) {
-    $scope.repos = [];
+    $scope.repos = []
+    $scope.repoNameList = []
 
     $http.get('https://api.github.com/users/mikebsg01/repos')
     .then(function(res) {
-      $scope.repos = res.data;
+      $scope.repos = res.data
+
+      for (i = 0; i < res.data.length; ++i) {
+        var name = res.data[i].name
+        $scope.repoNameList.push(name)
+      }
     }, function(res) {
       console.log(res)
     })
+
+    $scope.optionSelected = function(val) {
+      $scope.$apply(function() {
+        $scope.repoNameSearch = val
+      })
+    }
   }
 ])
--- index.php
+++ index.php
@@ -3,20 +3,34 @@
 <head>
   <meta charset="utf-8">
   <title>FirstController</title>
-  <link rel="stylesheet" type="text/css" href="http://materializecss.com/css/ghpages-materialize.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" type="text/css" href="style.css?v=<?php echo time(); ?>">
   <script type="text/javascript" src="angular.js?v=<?php echo time(); ?>"></script>
-  <script type="text/javascript" src="angular-local-storage.js?v=<?php echo time(); ?>"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-latest.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
+  <script type="text/javascript" src="autocomplete-jquery-ui.js?v=<?php echo time(); ?>"></script>
   <script type="text/javascript" src="controllers.js?v=<?php echo time(); ?>"></script>
 </head>
 <body ng-controller="AppCtrl">
 <div class="container">
   <div class="row">
-    <div class="col s10 offset-s1" ng-cloak>
+    <div class="col s12" ng-cloak>
       <h1 ng-show="repos.length > 0">{{ repos.length > 0 ? repos[0].owner.login : null }}<span class="new badge" data-badge-caption="repos">{{ repos.length }}</span></h1>
       <h5>GitHub Repos</h5>
+      <div class="col offset-s8 s4">
+        <form>
+          <div class="input-field col s12">
+          <i class="material-icons prefix">search</i>
+          <input id="repo_search" type="text" my-autocomplete="repoNameList">
+          <i ng-show="repoNameSearch" class="clear-input material-icons red-text" clear-input>close</i>
+          <label for="repo_search">Buscar un repositorio</label>
+        </div>
+        </form>
+      </div>
       <ul class="collection">
-        <li ng-repeat="repo in repos" class="collection-item avatar">
+        <li ng-repeat="repo in repos" ng-show="!repoNameSearch || repoNameSearch == repo.name" class="collection-item avatar">
           <a href="{{ repo.html_url }}">
             <img img-circle="{{ repo.owner.avatar_url }}">
           </a>
